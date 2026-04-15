720b07b730867d278edc006d2d8d80524ccb0bdb
cinnamoll <daq.for.work198@gmail.com>
2026-04-15 21:58:43 +0700

    daily

--- 1_Danny_Diner/A.sql
+++ 1_Danny_Diner/A.sql
@@ -76,3 +76,87 @@ FROM count_purchase
 WHERE ranking = 1;
 
 -- 6, Which item was purchased first by the customer after they became a member?
+WITH ranked AS (
+    SELECT
+        s.customer_id,
+        s.order_date,
+        m.product_name,
+        mm.join_date,
+        ROW_NUMBER() OVER(
+            PARTITION BY s.customer_id
+            ORDER BY s.order_date
+        ) AS ranking
+    FROM sales AS s
+    INNER JOIN menu AS m 
+    ON s.product_id = m.product_id
+    INNER JOIN members AS mm
+    ON mm.customer_id = s.customer_id
+    WHERE s.order_date >= mm.join_date 
+)
+SELECT 
+    customer_id,
+    order_date,
+    product_name,
+    join_date
+FROM ranked
+WHERE ranking = 1
+ORDER BY 1;
+
+
+-- 7, Which item was purchased just before the customer became a member?
+WITH ranked AS (
+    SELECT
+        s.customer_id,
+        s.order_date,
+        m.product_name,
+        mm.join_date,
+        ROW_NUMBER() OVER(
+            PARTITION BY s.customer_id
+            ORDER BY s.order_date DESC
+        ) AS ranking
+    FROM sales AS s
+    INNER JOIN menu AS m 
+    ON s.product_id = m.product_id
+    INNER JOIN members AS mm
+    ON mm.customer_id = s.customer_id
+    WHERE s.order_date <= mm.join_date 
+)
+SELECT 
+    customer_id,
+    order_date,
+    product_name,
+    join_date
+FROM ranked
+WHERE ranking = 1
+ORDER BY 1;
+
+-- 8, What is the total items and amount spent for each member before they became a member?
+SELECT 
+    s.customer_id,
+    COUNT(s.product_id) AS total_items,
+    SUM(m.price) AS total_amt
+FROM sales AS s
+INNER JOIN menu AS m 
+ON m.product_id = s.product_id
+INNER JOIN members AS mm
+ON mm.customer_id = s.customer_id
+WHERE s.order_date < mm.join_date
+GROUP BY s.customer_id;
+
+-- 9, If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
+SELECT 
+    s.customer_id,
+    SUM(points) AS total_points
+FROM sales AS s
+INNER JOIN (
+    SELECT
+        product_id,
+        CASE 
+            WHEN product_id = 1 THEN price * 20
+            ELSE price * 10 END 
+        AS points
+    FROM menu 
+) AS p
+ON s.product_id = p.product_id
+GROUP BY 1
+ORDER BY 1;
